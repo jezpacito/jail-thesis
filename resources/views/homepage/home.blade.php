5bfae20f74afcb2521eba927c829768d588c0e3a
unknown <ryahn33dichosazxc@gmail.com>
2021-05-15 20:28:40 +0800

    save

--- resources/views/homepage/home.blade.php
+++ resources/views/homepage/home.blade.php
@@ -127,6 +127,7 @@
 </div>
 
 
+
   <!-- About Section -->
   <div class="w3-container w3-padding-32" id="about">
     <h3 class="w3-border-bottom w3-border-light-grey w3-padding-16 text-center"><strong>About us</strong></h3>
