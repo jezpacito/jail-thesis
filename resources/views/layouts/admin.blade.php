4c87d2d4328ec3ed345f89d73b0fd04ec29e5513
unknown <ryahn33dichosazxc@gmail.com>
2021-05-15 20:57:20 +0800

    meta

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -15,7 +15,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
-
+    <meta> http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     {{--    icon--}}
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
           rel="stylesheet">
