604c11f7461b20c5ee2c8d7f0dfe7bab72c1a46b
unknown <ryahn33dichosazxc@gmail.com>
2021-05-16 03:10:14 +0800

    ui bug

--- resources/views/layouts/admin-logs.blade.php
+++ resources/views/layouts/admin-logs.blade.php
@@ -19,7 +19,7 @@
     <!-- Styles -->
     {{-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> --}}
 
-    <meta> http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     {{-- new --}}
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
